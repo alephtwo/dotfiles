7666666d24a8134c6c94cc739d02a28dfda56a47
Ben Christianson <thhuntertgm@gmail.com>
2018-04-30 16:13:28 -0500

    Syntax highlighting

--- apply.sh
+++ apply.sh
@@ -3,9 +3,9 @@
 ABS=$(pwd)
 
 # zsh
-# git clone git@github.com:zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting
 ln -sf $ABS/.zshrc $HOME/.zshrc
 ln -sf $ABS/zsh/themes/*.zsh-theme $HOME/.oh-my-zsh/themes/
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting
 
 # vim
 ln -sf $ABS/.vimrc $HOME/.vimrc
