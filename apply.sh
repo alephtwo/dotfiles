96e8cbb75a51ad3c354bfee16b4146251634dd8b
Ben Christianson <thhuntertgm@gmail.com>
2026-05-05 15:44:43 -0500

    fix apply

--- apply.sh
+++ apply.sh
@@ -1,17 +1,17 @@
-#! /usr/bin/env sh
-
-pwd=$(pwd)
+#! /usr/bin/env bash
+#
+cwd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # zsh
-ln -sf "${pwd}/.zshrc" "$HOME/.zshrc"
-ln -sf "${pwd}/.zshenv" "$HOME/.zshenv"
-ln -sf "${pwd}/zsh/themes/*.zsh-theme" "$HOME/.oh-my-zsh/themes/"
-git clone "https://github.com/zsh-users/zsh-syntax-highlighting.git" "$HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting"
+ln -sf "${cwd}/.zshrc" "$HOME/.zshrc"
+ln -sf "${cwd}/.zshenv" "$HOME/.zshenv"
+ln -sf "${cwd}/zsh/themes/aleph.zsh-theme" "$HOME/.oh-my-zsh/themes/aleph.zsh-theme"
+[[ ! -d "$HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting" ]] && git clone "https://github.com/zsh-users/zsh-syntax-highlighting.git" "$HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting"
 
 # vim
-ln -sf "${pwd}/.vimrc" "$HOME/.vimrc"
+ln -sf "${cwd}/.vimrc" "$HOME/.vimrc"
 
 # alacritty
 alacritty_config_dir="$HOME/.config/alacritty"
 [[ ! -d "${alacritty_config_dir}" ]] && mkdir -p "${alacritty_config_dir}"
-ln -sf "${pwd}/alacritty.toml" "${alacritty_config_dir}/alacritty.toml"
+ln -sf "${cwd}/alacritty.toml" "${alacritty_config_dir}/alacritty.toml"
